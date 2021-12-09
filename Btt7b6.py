c89be46bda2eda3a33a9d4c911e8743818b8e386
Tuan Canh <90490341+Tuancanh08@users.noreply.github.com>
2021-12-09 14:20:19 +0700

    Create Btt7b6.py

---
+++ Btt7b6.py
@@ -0,0 +1,9 @@
+import numpy as np
+X = np.array([2, 5, 3, 1, 7])
+Y = np.array([2, 1, 8, 5, 7, 9])
+
+print("Median X = ", np.median(X))
+print("Median Y = ", np.median(Y))
+
+#kq: Median X =  3.0
+#    Median Y =  6.0
