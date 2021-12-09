234d3c39949859523f4dc3bbfbe2fa784e892d64
Tuan Canh <90490341+Tuancanh08@users.noreply.github.com>
2021-12-09 14:22:08 +0700

    Create Btt7b8.py

---
+++ Btt7b8.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([[7, 4, 2], [3, 9, 5]])
+print("median arr (axis = 0) = ", np.median(arr, axis=0))
+print("median arr (axis = 1) = ", np.median(arr, axis=1))
+
+#kq: median arr (axis = 0) =  [5.  6.5 3.5]
+#    median arr (axis = 1) =  [4. 5.]
