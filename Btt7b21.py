e09c6ff7450ff01500cc3e33e69131781a89645c
Tuan Canh <90490341+Tuancanh08@users.noreply.github.com>
2021-12-09 14:30:29 +0700

    Create Btt7b21.py

---
+++ Btt7b21.py
@@ -0,0 +1,5 @@
+import numpy as np
+from skimage.data import coins
+
+print("% số lần tung được mặt ngửa: ", (coins == 0).mean() * 100)
+print("% số lần tung được mặt úp: ", (coins == 1).mean() * 100)
