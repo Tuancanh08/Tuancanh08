81934b3110c91388789f71a995229491f4b5c4ac
Tuan Canh <90490341+Tuancanh08@users.noreply.github.com>
2021-12-09 14:24:52 +0700

    Create Btt7b12.py

---
+++ Btt7b12.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+freetuts_ages = [19, 33, 51, 22, 18, 13, 45, 24, 58, 11, 25, 27, 26, 29]
+
+print("Phương sai: ", np.var(freetuts_ages))
+print("Độ lệch chuẩn: ", np.std(freetuts_ages))
+
+#kq: Phương sai:  178.51530612244895
+#    Độ lệch chuẩn:  13.360962020844493
