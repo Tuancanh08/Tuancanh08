b56ff14736e289ba978723f3f920eff36264ba9e
Tuan Canh <90490341+Tuancanh08@users.noreply.github.com>
2021-12-09 14:21:31 +0700

    Create Btt7b7.py

---
+++ Btt7b7.py
@@ -0,0 +1,5 @@
+import numpy as np
+freetuts_visitors = np.array([3776, 3112, 3476, 3319, 3559, 2121, 3462])
+print("Số người truy cập trung bình mỗi ngày trong tuần qua của Freetuts: ", np.mean(freetuts_visitors))
+
+#kq: Số người truy cập trung bình mỗi ngày trong tuần qua của Freetuts:  3260.714285714286
