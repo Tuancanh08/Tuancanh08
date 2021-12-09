20e485aab1e5cf5e56de0cfe6f4c12adc2cf0461
Tuan Canh <90490341+Tuancanh08@users.noreply.github.com>
2021-12-09 14:33:24 +0700

    Create Btt7b26.py

---
+++ Btt7b26.py
@@ -0,0 +1,4 @@
+import numpy as np
+print("Trung bình số mặt ngửa nhận được khi tung đồng xu 20 lần trong vòng 10 lần lặp: ", np.random.binomial(20, 0.5, 10).mean())
+
+#kq: Trung bình số mặt ngửa nhận được khi tung đồng xu 20 lần trong vòng 10 lần lặp:  9.4
