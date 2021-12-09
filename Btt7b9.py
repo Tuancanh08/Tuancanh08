b27d46be4c7fd4ed55c51eb7530e7e3a21d38302
Tuan Canh <90490341+Tuancanh08@users.noreply.github.com>
2021-12-09 14:22:49 +0700

    Create Btt7b9.py

---
+++ Btt7b9.py
@@ -0,0 +1,7 @@
+import numpy as np
+freetuts_visitors = np.array([3776, 3112, 3476, 3319, 3559, 50293, 30432]) # 2 giá trị cuối lệch xa so với các giá trị trong dãy
+print("Mean = : ", np.mean(freetuts_visitors))
+print("Median = : ", np.median(freetuts_visitors))
+
+#kq: Mean = :  13995.285714285714
+#    Median = :  3559.0
